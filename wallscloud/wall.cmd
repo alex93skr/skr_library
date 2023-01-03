2f6662499b073bd57e95d309b58d80e344ce4836
alex93skr <dendyaev@gmail.com>
2023-01-03 14:35:51 +0300

    up

---
+++ wallscloud/wall.cmd
@@ -0,0 +1,3 @@
+@echo off
+
+C:\Code\Python\Python311\pythonw.exe D:\Code\skr_library\wallscloud\wallscloud.py
--- wallscloud/wallscloud.py
+++ wallscloud/wallscloud.py
@@ -1,6 +1,8 @@
 import requests, argparse, time
 import ctypes, os, random
 
+WALLPAPER_FILE = 'D:\\Code\\skr_library\\wallscloud\\wallpaper.jpg'
+
 
 def download_wallpaper():
     random_url = 'https://wallscloud.net/ru/api_app/wallpapers?&offset=0&limit=10&sort=random'
@@ -8,24 +10,23 @@ def download_wallpaper():
 
     for i in random_request.json():
         if int(i['width']) >= 1920 and int(i['height']) >= 1080:
-            print(i['id'], i['width'], i['height'])
+            # print(i['id'], i['width'], i['height'])
             id_for_download = i['id']
 
     wallpaper_url = f'https://wallscloud.net/ru/api_app/wallpaper/{id_for_download}'
     wallpaper_request = requests.get(wallpaper_url)
 
-    print(wallpaper_request.json()['page_Url'])
+    # print(wallpaper_request.json()['page_Url'])
 
     download_url = f"{wallpaper_request.json()['page_Url']}/1920x1080/download"
 
     wallpaper_request = requests.get(download_url)
-    with open('wallpaper.jpg', 'wb') as f:
+    with open(WALLPAPER_FILE, 'wb') as f:
         f.write(wallpaper_request.content)
 
 
 def set_wallpaper():
-    path = f'{os.getcwd()}\wallpaper.jpg'
-    ctypes.windll.user32.SystemParametersInfoW(20, 0, path, 0)
+    ctypes.windll.user32.SystemParametersInfoW(20, 0, WALLPAPER_FILE, 0)
 
 
 # time.sleep(3)
